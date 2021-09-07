10b59f961dda37891e13017a076be42989ec5adb
tristan <tristan.lee@qq.com>
2021-09-07 10:22:54 +0800

    fix debug info

--- client/consensus/manual-seal/src/seal_block.rs
+++ client/consensus/manual-seal/src/seal_block.rs
@@ -134,7 +134,7 @@ pub async fn seal_block<B, BI, SC, C, E, P>(
 
 		// add custom block-hash
 		if let Some(hash) = hash {
-			log::warn!("******manual-seal block-hash {:?}", hash);
+			//log::warn!("******manual-seal block-hash {:?}", hash);
 			const CUSTOM_HEADER_HASH_ID: ConsensusEngineId = *b"cust";
 			digest.push(DigestItem::Seal(CUSTOM_HEADER_HASH_ID, hash.encode()));
 		}
